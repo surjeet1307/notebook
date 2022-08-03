47b0af4e088c9095a57577897290a28150ea59d7
surjeet1307 <surjeetsingh130799@gmail.com>
2022-08-03 14:45:52 +0530

    complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ function About() {
    
   return (
     <div className='container mt-5'>
-      <div class="card">
+      <div className="card">
       <h1 className='text-center'>About Us</h1>
-  <div class="card-body">
+  <div className="card-body">
   Welcome to iNoteBook. We're dedicated to giving you the very best of product, with a focus on three characteristics, ie: dependability, customer service and uniqueness.
 Founded in 2022 by SURJEET SINGH. When Website founder first started out, his/her passion for passion of founder, ie: helping other parents be more eco-friendly, providing the best notes web app drove him to action, ie: do intense research, quit her day job, and gave him the impetus to turn hard work and inspiration into to a booming online store. We now serve customers all over place, ie: the US, the world, the Austin area.
 
